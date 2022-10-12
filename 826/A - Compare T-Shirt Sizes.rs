7c12e69697f2b8005ecb105555641e135539c322
Chris Ohk <utilforever@gmail.com>
2022-10-12 21:18:22 +0900

    Upload 826 "A - Compare T-Shirt Sizes"

---
+++ 826/A - Compare T-Shirt Sizes.rs
@@ -0,0 +1,74 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<usize>();
+
+    for _ in 0..t {
+        let (a, b) = (scan.token::<String>(), scan.token::<String>());
+
+        let a = a.chars().collect::<Vec<_>>();
+        let b = b.chars().collect::<Vec<_>>();
+
+        let last_a = a.iter().last().unwrap();
+        let last_b = b.iter().last().unwrap();
+
+        if last_a == last_b {
+            if last_a == &'S' {
+                writeln!(out, "{}", if a.len() > b.len() { "<" } else if a.len() < b.len() { ">" } else { "=" }).unwrap();
+            } else if last_a == &'L' {
+                writeln!(out, "{}", if a.len() > b.len() { ">" } else if a.len() < b.len() { "<" } else { "=" }).unwrap();
+            } else {
+                writeln!(out, "=").unwrap();
+            }
+        } else {
+            if last_a == &'S' {
+                writeln!(out, "<").unwrap();
+            } else if last_a == &'L' {
+                writeln!(out, ">").unwrap();
+            } else {
+                if last_b == &'S' {
+                    writeln!(out, ">").unwrap();
+                } else {
+                    writeln!(out, "<").unwrap();
+                }
+            }
+        }
+    }
+}
