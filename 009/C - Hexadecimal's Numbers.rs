015f0f2a12a0aa95db955f4b792a45df1c526260
Chris Ohk <utilforever@gmail.com>
2022-10-16 14:36:58 +0900

    Upload 009 "C - Hexadecimal's Numbers"

---
+++ 009/C - Hexadecimal's Numbers.rs
@@ -0,0 +1,58 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn process_dfs(num: i64, n: i64, ret: &mut i64) {
+    if num > n {
+        return;
+    }
+
+    *ret += 1;
+
+    process_dfs(num * 10, n, ret);
+    process_dfs(num * 10 + 1, n, ret);
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let n = scan.token::<i64>();
+    let mut ret = 0;
+
+    process_dfs(1, n, &mut ret);
+
+    writeln!(out, "{ret}").unwrap();
+}
