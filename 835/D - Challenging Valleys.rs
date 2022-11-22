e2e7721cff662c8631667d7d44c1659993a242c0
Chris Ohk <utilforever@gmail.com>
2022-11-22 22:58:44 +0900

    Upload 835 "D - Challenging Valleys"

---
+++ 835/D - Challenging Valleys.rs
@@ -0,0 +1,73 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let n = scan.token::<usize>();
+        let mut arr = vec![1_000_000_007; n + 2];
+
+        for i in 1..=n {
+            arr[i] = scan.token::<i64>();
+        }
+
+        let mut left = 1;
+        let mut right = 1;
+        let mut ret = 0;
+
+        while left <= n && right <= n {
+            if arr[right] == arr[right + 1] {
+                right += 1;
+                continue;
+            }
+
+            if arr[left - 1] > arr[left] && arr[right] < arr[right + 1] {
+                ret += 1;
+            }
+
+            left = right;
+
+            left += 1;
+            right += 1;
+        }
+
+        writeln!(out, "{}", if ret == 1 { "YES" } else { "NO" }).unwrap();
+    }
+}
