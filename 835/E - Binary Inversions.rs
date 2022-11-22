3486984788c22a156363a2afdf6fc0830c667b4d
Chris Ohk <utilforever@gmail.com>
2022-11-23 02:26:51 +0900

    Upload 835 "E - Binary Inversions"

---
+++ 835/E - Binary Inversions.rs
@@ -0,0 +1,105 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+
+    for _ in 0..t {
+        let n = scan.token::<usize>();
+        let mut arr = vec![0; n];
+
+        for i in 0..n {
+            arr[i] = scan.token::<i64>();
+        }
+
+        let is_all_same = arr.iter().all(|&x| x == arr[0]);
+
+        if is_all_same {
+            writeln!(out, "{}", n - 1).unwrap();
+            continue;
+        }
+
+        let mut prefix_sum = vec![0; n];
+        let mut cnt = 0;
+ 
+        for i in (0..n).rev() {
+            if arr[i] == 0 {
+                cnt += 1;
+            } else {
+                prefix_sum[i] = cnt;
+            }
+        }
+
+        let mut ret = prefix_sum.iter().sum::<i64>();
+        
+        let mut arr2 = arr.clone();
+        arr2.iter().position(|&x| x == 0).map(|x| arr2[x] = 1);
+
+        let mut prefix_sum = vec![0; n];
+        let mut cnt = 0;
+ 
+        for i in (0..n).rev() {
+            if arr2[i] == 0 {
+                cnt += 1;
+            } else {
+                prefix_sum[i] = cnt;
+            }
+        }
+
+        ret = ret.max(prefix_sum.iter().sum::<i64>());
+
+        let mut arr2 = arr.clone();
+        arr2.iter().rposition(|&x| x == 1).map(|x| arr2[x] = 0);
+
+        let mut prefix_sum = vec![0; n];
+        let mut cnt = 0;
+ 
+        for i in (0..n).rev() {
+            if arr2[i] == 0 {
+                cnt += 1;
+            } else {
+                prefix_sum[i] = cnt;
+            }
+        }
+
+        ret = ret.max(prefix_sum.iter().sum::<i64>());
+
+        writeln!(out, "{ret}").unwrap();
+    }
+}
