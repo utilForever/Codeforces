6344bec052ce2af8e2ec67db1292768b10a18db3
Chris Ohk <utilforever@gmail.com>
2023-01-28 21:02:51 +0900

    Upload 847 "A - Polycarp and the Day of Pi"

---
+++ 847/A - Polycarp and the Day of Pi.rs
@@ -0,0 +1,60 @@
+use io::Write;
+use std::{io, str};
+
+pub struct UnsafeScanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: io::BufRead> UnsafeScanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn token<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = UnsafeScanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let t = scan.token::<i64>();
+    let pi = "3141592653589793238462643383279502884197169399375105820974944592307816406"
+        .chars()
+        .collect::<Vec<char>>();
+
+    for _ in 0..t {
+        let n = scan.token::<String>();
+        let n = n.chars().collect::<Vec<char>>();
+        let mut ret = n.len();
+
+        for i in 0..n.len() {
+            if n[i] != pi[i] {
+                ret = i;
+                break;
+            }
+        }
+
+        writeln!(out, "{ret}").unwrap();
+    }
+}
